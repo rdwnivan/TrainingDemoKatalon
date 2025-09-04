3bf79fe2dcb78bf368d7f91e0f3e0231593ba657
ridwa <ridwa@192.168.1.191>
2025-09-05 00:08:18 +0700

    New Demo test Suite 2

---
+++ Test Suites/TestSuiteDemo2.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuiteDemo2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>6eb3d210-4e96-4a28-ab08-c387bb24d912</testSuiteGuid>
+   <testCaseLink>
+      <guid>a18eaa97-42d4-4d5d-bb47-ce9b944d47c7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/OrangeTestcase</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>a4418323-556a-4fec-bbe9-1be9eb01f1aa</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
